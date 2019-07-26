d11043e2765e46ad1ab275704bbda7cf3939feef
JLopez <jessealexanderlopez222@gmail.com>
2019-07-26 16:01:59 -0400

    -Color scheme changes

--- src/app/components/games/games.component.ts
+++ src/app/components/games/games.component.ts
@@ -24,20 +24,19 @@ export class GamesComponent implements OnInit {
   }
 
   onGameDrop(event: CdkDragDrop<string[]>) {
-    // console.log("prev " + event.previousIndex + " " + this.games[event.previousIndex].name + " curr " + event.currentIndex + " " + this.games[event.currentIndex].name);
+    console.log("prev " + event.previousIndex + " " + this.games[event.previousIndex].name + " curr " + event.currentIndex + " " + this.games[event.currentIndex].name);
 
     moveItemInArray(this.games, event.previousIndex, event.currentIndex);
 
-    this.swapGameId(event.previousIndex, event.currentIndex);
+    this.udpateGameId();
 
     this.gameService.setGames(this.games);
   }
 
-  swapGameId(prev:number,curr:number):void{
-    var tempGameId = this.games[prev].id;
-
-    this.games[prev].id = this.games[curr].id;
-    this.games[curr].id = tempGameId;
+  udpateGameId():void{
+    this.games.forEach((game,index)=> {
+      game.id = index+1;
+    });
   }
 
 }
